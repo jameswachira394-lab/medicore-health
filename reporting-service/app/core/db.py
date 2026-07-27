2908484b00fd2dd6f1b2694251d0d911b0554513
Dr 90923 <jameswachira394@gmail.com>
2026-07-27 11:33:41 +0300

    Delete reporting-service directory

--- reporting-service/app/core/db.py
+++
@@ -1,7 +0,0 @@
-from shared_common.database import Base, make_engine, make_session_factory, get_db_dependency
-
-from app.core.config import settings
-
-engine = make_engine(settings.DATABASE_URL)
-SessionLocal = make_session_factory(engine)
-get_db = get_db_dependency(SessionLocal)
