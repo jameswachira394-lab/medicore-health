c458fa5d768cd7ffe689732b80abf5b494306f03
Dr 90923 <jameswachira394@gmail.com>
2026-07-27 11:32:18 +0300

    Delete billing-service directory

--- billing-service/app/core/config.py
+++
@@ -1,9 +0,0 @@
-from shared_common.config import BaseServiceSettings
-
-
-class Settings(BaseServiceSettings):
-    SERVICE_NAME: str = "billing-service"
-    PAYMENT_GATEWAY_API_KEY: str = ""
-
-
-settings = Settings()
